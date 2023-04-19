f249a3440dbe48510991a34abc10a710aa691b29
Basharat, Basharat <basharat.martin@noaa.gov>
2023-04-18 22:02:02 -0400

    artemis_piston :
    modified and updated the artemis_piston initialization, added
    extra (0x00) byte to the command to the pistonboard

--- Firmware/LCP_Control/src/artemis/artemis_piston.c
+++ Firmware/LCP_Control/src/artemis/artemis_piston.c
@@ -100,10 +100,9 @@ static module_t module;
 void artemis_piston_i2c_initialize(uint8_t i2c_addr)
 {
     artemis_i2c_t *i2c = &module.i2c;
-    module.power.pinConfig = (am_hal_gpio_pincfg_t *)&g_AM_BSP_GPIO_GPS_ON;
+    //module.power.pinConfig = (am_hal_gpio_pincfg_t *)&g_AM_BSP_GPIO_GPS_ON;
+    module.power.pinConfig = (am_hal_gpio_pincfg_t *)&g_AM_BSP_GPIO_PWR_CTRL_EN;
     module.power.pin = AM_BSP_GPIO_PWR_CTRL_EN;
-    
-
 
     i2c->address = i2c_addr;
     i2c->iom.module = 2;
@@ -111,51 +110,49 @@ void artemis_piston_i2c_initialize(uint8_t i2c_addr)
     i2c->iom.config.ui32ClockFreq = AM_HAL_IOM_400KHZ;
     artemis_iom_initialize(&i2c->iom);
 
+    ARTEMIS_DEBUG_HALSTATUS(am_hal_gpio_pinconfig(AM_BSP_GPIO_IOM2_SCL, g_AM_BSP_GPIO_IOM2_SCL));
+    ARTEMIS_DEBUG_HALSTATUS(am_hal_gpio_pinconfig(AM_BSP_GPIO_IOM2_SDA, g_AM_BSP_GPIO_IOM2_SDA));
     ARTEMIS_DEBUG_HALSTATUS(am_hal_gpio_pinconfig(module.power.pin, *module.power.pinConfig));
+
     artemis_piston_i2c_power_off();
     am_hal_systick_delay_us(1000);
     artemis_piston_i2c_power_on();
-
-
-    ARTEMIS_DEBUG_HALSTATUS(am_hal_gpio_pinconfig(AM_BSP_GPIO_IOM2_SCL, g_AM_BSP_GPIO_IOM2_SCL));
-    ARTEMIS_DEBUG_HALSTATUS(am_hal_gpio_pinconfig(AM_BSP_GPIO_IOM2_SDA, g_AM_BSP_GPIO_IOM2_SDA));
-    
-
 }
 
 void artemis_piston_i2c_power_on(void)
 {
-    am_hal_gpio_state_write(module.power.pin, AM_HAL_GPIO_OUTPUT_SET);
+    am_hal_gpio_output_set(module.power.pin);
 }
 
 void artemis_piston_i2c_power_off(void)
 {
-    am_hal_gpio_state_write(module.power.pin, AM_HAL_GPIO_OUTPUT_CLEAR);
+    am_hal_gpio_output_clear(module.power.pin);
 }
 
 void artemis_piston_i2c_send_msg(uint8_t *msg, uint16_t len, bool stop)
 {
-  
-  
+
     artemis_i2c_t *i2c = &module.i2c;
-  artemis_stream_t txstream = {0};
-  artemis_stream_setbuffer(&txstream, module.txbuffer, ARTEMIS_PISTON_BUFFER_LENGTH);
-  artemis_stream_reset(&txstream);
-  
-  while(len > 0)
-  {
-    if(len > ARTEMIS_PISTON_BUFFER_LENGTH)
+    artemis_stream_t txstream = {0};
+    artemis_stream_setbuffer(&txstream, module.txbuffer, ARTEMIS_PISTON_BUFFER_LENGTH);
+    artemis_stream_reset(&txstream);
+
+    while(len > 0)
     {
-      artemis_stream_write(&txstream, msg, ARTEMIS_PISTON_BUFFER_LENGTH);
-      artemis_i2c_send(i2c, false, &txstream);
-      artemis_stream_reset(&txstream);
-      len -= ARTEMIS_PISTON_BUFFER_LENGTH;
-    } else {
-      artemis_stream_write(&txstream, msg, len);
-      artemis_i2c_send(i2c, stop, &txstream);
-      len =0;
+        if(len > ARTEMIS_PISTON_BUFFER_LENGTH)
+        {
+            artemis_stream_write(&txstream, msg, ARTEMIS_PISTON_BUFFER_LENGTH);
+            artemis_i2c_send(i2c, true, &txstream);
+            artemis_stream_reset(&txstream);
+            len -= ARTEMIS_PISTON_BUFFER_LENGTH;
+        }
+        else
+        {
+            artemis_stream_write(&txstream, msg, len);
+            artemis_i2c_send(i2c, stop, &txstream);
+            len = 0;
+        }
     }
-  }
   
 //  while(len-- > 0)
 //  {
@@ -242,25 +239,23 @@ void artemis_piston_i2c_read(uint8_t addr, uint8_t *data, uint8_t len)
 
     /** Send the command to retreive data length @addr 0xFD */
     artemis_stream_put(&txstream, ARTEMIS_PISTON_BUFFER_LENGTH);
-    artemis_i2c_send(i2c, false, &txstream);
+    artemis_i2c_send(i2c, true, &txstream);
 
     artemis_i2c_receive(i2c, true, &rxstream, len);
-
     artemis_stream_read(&rxstream, data, len);
 }
 
 
 void artemis_piston_set_write_mode(bool state)
 {
-  uint8_t key = 0x00;
-  uint8_t msg[2] = {0x07, 0x00};
-  if(state)
-  {
-    key = 0xA5;
-  }
-  msg[1] = key;
-  
-  artemis_piston_i2c_send_msg(msg, 1, false);
-  artemis_piston_i2c_send_msg(msg, 2, true);
-  am_hal_systick_delay_us(500000);
+    uint8_t key = 0x00;
+    uint8_t msg[3] = {0x07, 0x00, 0x00};
+    if(state)
+    {
+        key = 0xA5;
+    }
+    msg[1] = key;
+
+    artemis_piston_i2c_send_msg(msg, 3, true);
+    am_hal_systick_delay_us(500000);
 }
