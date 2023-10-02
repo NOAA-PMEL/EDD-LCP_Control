b81c27b6e7e321dd3612153311427654f4f5c385
Basharat, Basharat <basharat.martin@noaa.gov>
2023-10-02 16:24:53 -0400

    sysinfo : modified and added more functions regarding LCP firmware number,           build-date, serial-number and so

--- Firmware/LCP_Control/include/sys/sysinfo.h
+++ Firmware/LCP_Control/include/sys/sysinfo.h
@@ -3,27 +3,48 @@
 
 #include <stdint.h>
 
-// #ifdef TEST
-#define SERIAL_NUMBER   ( 0x3C )
-#define FIRMWARE_MAJOR  ( 0x01 )
-#define FIRMWARE_MINOR  ( 0x32 )
-#define FIRMWARE_BUILD  ( "a32b89" )
-// #endif 
+#define SYSTEM_ID       ( 0x4D )        /** < NOAA system ID, 0x4D = 77 */
+
+#define FIRMWARE_MAJOR  ( 0x0 )
+#define FIRMWARE_MINOR  ( 0x0 )
+#define FIRMWARE_PATCH  ( 0x0 )
+
+#define SERIAL_NUMBER   ( 0x4C0000 )    /** < Serial number always starts with L = LCP,
+                                              rest two bytes can be used to differentiate
+                                              65535 LCPS profilers , LCP variant can be
+                                              used as well */
+
+#define LCP_VARIANT     ( 0x00 )        /** < LCP variant defines the number of sensors
+                                              attached to the LCP profiler */
+
+static const char *const build_date = __DATE__;
+static const char *const build_time = __TIME__;
 
 typedef struct sSysInfo_t
 {
-    uint16_t serial_number;     /**< System Serial Number */
-    struct {
-        uint8_t major;          /**< Firmware Major Number */
-        uint8_t minor;          /**< Firmware Minor Number */
-        uint8_t build[6];       /**< Firmware Build Number */
-    }firmware;
-}SysInfo_t;
+    uint8_t system_id ;
+    uint32_t serial_number: 24;     /**< System Serial Number */
+    struct
+    {
+        uint8_t major:  4;          /**< Firmware Major Number */
+        uint8_t minor:  4;          /**< Firmware Minor Number */
+        uint8_t patch:  8;          /**< Firmware Patch Number */
+    } firmware;
+    uint8_t lcp_variant;
+    struct
+    {
+        uint8_t year:   7;
+        uint8_t month:  4;
+        uint8_t day:    5;
+    } build_date;
 
+} __attribute__((packed)) SysInfo_t ;
 
-
-
-uint16_t SYS_get_serial_num(void);
-void SYS_get_firmware( uint8_t *major, uint8_t *minor, uint8_t *build);
+uint16_t SYS_get_build_year_date(void);
+uint32_t SYS_get_serial_num(void);
+uint16_t SYS_get_firmware(void);
+uint8_t SYS_get_system_id(void);
+uint8_t SYS_get_lcp_variant(void);
+void SYS_lcp_info(void);
 
 #endif // SYSINFO_H
--- Firmware/LCP_Control/src/sys/sysinfo.c
+++ Firmware/LCP_Control/src/sys/sysinfo.c
@@ -1,29 +1,76 @@
 #include "sysinfo.h"
-#include <string.h>
-
+#include "artemis_rtc.h"
+#include "artemis_debug.h"
 
 static SysInfo_t System = {
+
+    .system_id = SYSTEM_ID,
     .serial_number = SERIAL_NUMBER,
     .firmware.major = FIRMWARE_MAJOR,
     .firmware.minor = FIRMWARE_MINOR,
-    .firmware.build = FIRMWARE_BUILD,
+    .firmware.patch = FIRMWARE_PATCH,
+    .lcp_variant = LCP_VARIANT,
 };
 
-
-
-
-
-
-
-
-uint16_t SYS_get_serial_num(void)
+uint32_t SYS_get_serial_num(void)
 {
     return System.serial_number;
 }
 
-void SYS_get_firmware( uint8_t *major, uint8_t *minor, uint8_t *build)
+uint16_t SYS_get_firmware(void)
 {
-    *major = System.firmware.major;
-    *minor = System.firmware.minor;
-    strncpy(build, System.firmware.build, 6);
+    uint16_t ver = 0x00;
+    ver |= System.firmware.major << 12;
+    ver |= System.firmware.minor << 8;
+    ver |= System.firmware.patch ;
+
+    return ver;
+}
+
+uint16_t SYS_get_build_year_date(void)
+{
+    uint16_t date = 0x0000;
+
+    System.build_date.day = toVal(&build_date[4]);
+    System.build_date.month = mthToIndex(&build_date[0]);
+    System.build_date.year = toVal(&build_date[9]);
+
+    date |= (System.build_date.year << 9);
+    date |= (System.build_date.month << 5);
+    date |= (System.build_date.day);
+
+    return date;
+}
+
+uint8_t SYS_get_system_id(void)
+{
+    return System.system_id;
+}
+
+uint8_t SYS_get_lcp_variant(void)
+{
+    return System.lcp_variant;
+}
+
+void SYS_lcp_info(void)
+{
+    ARTEMIS_DEBUG_PRINTF("\n\nLCP Profiler Information\n");
+    ARTEMIS_DEBUG_PRINTF("*****************************\n");
+
+    uint8_t lcp_id = SYS_get_system_id();
+    ARTEMIS_DEBUG_PRINTF("\tSystem ID\t: %u\n", lcp_id);
+
+    uint16_t lcp_fw = SYS_get_firmware();
+    ARTEMIS_DEBUG_PRINTF("\tLCP Firmware\t: %u.%u.%u-dev\n", lcp_fw>>12&0xF, lcp_fw>>8&0xF, lcp_fw&0xFF);
+
+    uint16_t fw_date = SYS_get_build_year_date();
+    ARTEMIS_DEBUG_PRINTF("\tFirmware Date\t: %u.%u.%u\n", fw_date>>5&0xF, fw_date&0x1F,  fw_date>>9&0x7F);
+    ARTEMIS_DEBUG_PRINTF("\tFirmware Time\t: %s\n", build_time);
+
+    uint8_t lcp_variant = SYS_get_lcp_variant();
+    ARTEMIS_DEBUG_PRINTF("\tLCP Variant\t: %u\n", lcp_variant);
+
+    uint32_t lcp_ser = SYS_get_serial_num();
+    ARTEMIS_DEBUG_PRINTF("\tLCP Serial\t: %c%u%u\n", lcp_ser>>16&0xFF, lcp_ser>>8&0xFF, lcp_ser&0xFF);
+    ARTEMIS_DEBUG_PRINTF("\n");
 }
