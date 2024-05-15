f4f555c5419baf938a96f0240992f86b8466c65a
Basharat, Basharat <basharat.martin@noaa.gov>
2024-05-15 15:44:11 -0400

    FreeRTOSConfig : increased the HEAP Memory up to 128K

--- Firmware/LCP_Control/include/RTOS/FreeRTOSConfig.h
+++ Firmware/LCP_Control/include/RTOS/FreeRTOSConfig.h
@@ -68,7 +68,7 @@ extern "C"
 #define configTICK_RATE_HZ                      1000
 #define configMAX_PRIORITIES                    8
 #define configMINIMAL_STACK_SIZE                (256)
-#define configTOTAL_HEAP_SIZE                   (96 * 1024)
+#define configTOTAL_HEAP_SIZE                   (128 * 1024)
 #define configMAX_TASK_NAME_LEN                 32
 #define configUSE_16_BIT_TICKS                  0
 #define configIDLE_SHOULD_YIELD                 1
