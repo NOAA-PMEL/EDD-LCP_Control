2e5e54b3d3a93539df89f5b0820f4b1aca96afc9
jkurina86 <124196947+jkurina86@users.noreply.github.com>
2025-01-25 01:33:17 +0000

    Successfully built and tested
    flash write and read operations work.

---
+++ Firmware/LCP_Control/include/flash.h
@@ -0,0 +1,80 @@
+/** @file flash.h
+ *  @brief Flash memory library
+ *
+ *  @author Joseph Kurina, joseph.kurina@noaa.gov
+ *  @date January 17, 2025
+ *  @version 0.0.1
+ *
+ *  @copyright National Oceanic and Atmospheric Administration
+ *  @copyright Pacific Marine Environmental Lab
+ *  @copyright Environmental Development Division
+ *
+ *  @note
+ *
+ *  @bug  No known bugs
+ */
+
+#ifndef FLASH_H
+#define FLASH_H
+
+#include <stdint.h>
+#include <stddef.h>
+
+// Return codes for Flash operations
+#define FLASH_SUCCESS 0
+#define FLASH_ERROR   -1
+#define FLASH_INVALID_PARAM -2 // Add explicit error for invalid parameters
+
+// Define start and size of NVSTORAGE
+#define FLASH_NVSTORAGE_START 0x0004A000 // Per the linker file, this is the start of the NVSTORAGE region.
+#define FLASH_NVSTORAGE_SIZE  (660 * 1024)
+
+// Flash alignment constraints
+#define FLASH_ALIGNMENT 4
+
+/**
+ * @brief Write data to NVSTORAGE.
+ *
+ * @param data Pointer to the data to write.
+ * @param size Size of the data in bytes. Must be a multiple of FLASH_ALIGNMENT.
+ * @param offset Offset within NVSTORAGE to start writing.
+ * @return FLASH_SUCCESS on success, FLASH_ERROR or FLASH_INVALID_PARAM on failure.
+ */
+int flash_write(const void *data, size_t size, uint32_t offset);
+
+/**
+ * @brief Erase a region in NVSTORAGE.
+ *
+ * @param offset Offset within NVSTORAGE to start erasing.
+ * @param size Size of the region to erase in bytes. Should align with Flash page size.
+ * @return FLASH_SUCCESS on success, FLASH_ERROR or FLASH_INVALID_PARAM on failure.
+ */
+int flash_erase(uint32_t offset, size_t size);
+
+/**
+ * @brief Read data from NVSTORAGE.
+ *
+ * @param buffer Pointer to the buffer to store the data.
+ * @param size Size of the data to read in bytes.
+ * @param offset Offset within NVSTORAGE to start reading.
+ * @return FLASH_SUCCESS on success, FLASH_ERROR or FLASH_INVALID_PARAM on failure.
+ */
+int flash_read(void *buffer, size_t size, uint32_t offset);
+
+/**
+ * @brief Validate if the given offset and size are within the NVSTORAGE bounds.
+ *
+ * @param offset Offset within NVSTORAGE.
+ * @param size Size of the operation.
+ * @return 1 if valid, 0 otherwise.
+ */
+int flash_is_valid_range(uint32_t offset, size_t size);
+
+/**
+ * @brief Get the Flash page size.
+ *
+ * @return Size of a Flash page in bytes.
+ */
+size_t flash_get_page_size(void);
+
+#endif // FLASH_H
--- Firmware/LCP_Control/main.c
+++ Firmware/LCP_Control/main.c
@@ -20,8 +20,10 @@
 #include "main.h"
 #include "sensors.h"
 #include "StateMachine.h"
+#include "flash.h"
+#include <string.h>
 
-#define LCP_FREE_RTOS
+//#define LCP_FREE_RTOS
 //#define SENSORS_Test
 
 int main(void)
@@ -69,7 +71,7 @@ int main(void)
 
     ARTEMIS_DEBUG_PRINTF("\n*****************************\n");
     ARTEMIS_DEBUG_PRINTF("FreeRTOS here\n");
-    ARTEMIS_DEBUG_PRINTF("scheduler is going to start\n");
+    ARTEMIS_DEBUG_PRINTF("schedular is going to start\n");
     ARTEMIS_DEBUG_PRINTF("*****************************\n\n");
     vTaskStartScheduler();
     ARTEMIS_DEBUG_PRINTF("Do not get here\n");
@@ -82,12 +84,66 @@ int main(void)
     configASSERT(xTaskCreate( (TaskFunction_t) SENS_Test, "Sensor_Test_Task", 256, NULL, tskIDLE_PRIORITY + 3UL, NULL) == pdPASS);
 #endif
 
+#ifdef LCP_FREE_RTOS
     ARTEMIS_DEBUG_PRINTF("\n*****************************\n");
     ARTEMIS_DEBUG_PRINTF("FreeRTOS here\n");
     ARTEMIS_DEBUG_PRINTF("scheduler is going to start\n");
     ARTEMIS_DEBUG_PRINTF("*****************************\n\n");
     vTaskStartScheduler();
     ARTEMIS_DEBUG_PRINTF("Do not get here\n");
+#endif
+
+    // Test flash operations
+    ARTEMIS_DEBUG_PRINTF("\nTesting Flash Operations\n");
+
+    uint32_t test_data[4] = {0x12345678, 0x9ABCDEF0, 0xDEADBEEF, 0xFEEDC0DE}; // Data to write
+    uint32_t read_buffer[4] = {0}; // Buffer to read back data
+    uint32_t test_offset = 0x100; // Offset within NVSTORAGE for testing
+
+    // 1. Erase the flash memory at the specified offset
+    ARTEMIS_DEBUG_PRINTF("Erasing Flash at offset 0x%X...\n", test_offset);
+    if (flash_erase(test_offset, sizeof(test_data)) == FLASH_SUCCESS)
+    {
+        ARTEMIS_DEBUG_PRINTF("Flash erase successful.\n");
+    }
+    else
+    {
+        ARTEMIS_DEBUG_PRINTF("Flash erase failed.\n");
+    }
+
+    // 2. Write data to flash memory
+    ARTEMIS_DEBUG_PRINTF("Writing data to Flash at offset 0x%X...\n", test_offset);
+    if (flash_write(test_data, sizeof(test_data), test_offset) == FLASH_SUCCESS)
+    {
+        ARTEMIS_DEBUG_PRINTF("Flash write successful.\n");
+    }
+    else
+    {
+        ARTEMIS_DEBUG_PRINTF("Flash write failed.\n");
+    }
+
+    // 3. Read data back from flash memory
+    ARTEMIS_DEBUG_PRINTF("Reading data from Flash at offset 0x%X...\n", test_offset);
+    if (flash_read(read_buffer, sizeof(read_buffer), test_offset) == FLASH_SUCCESS)
+    {
+        ARTEMIS_DEBUG_PRINTF("Flash read successful.\n");
+        ARTEMIS_DEBUG_PRINTF("Read Data: 0x%08X 0x%08X 0x%08X 0x%08X\n",
+                             read_buffer[0], read_buffer[1], read_buffer[2], read_buffer[3]);
+    }
+    else
+    {
+        ARTEMIS_DEBUG_PRINTF("Flash read failed.\n");
+    }
+
+    // Verify the data
+    if (memcmp(test_data, read_buffer, sizeof(test_data)) == 0)
+    {
+        ARTEMIS_DEBUG_PRINTF("Data verification successful: Read data matches written data.\n");
+    }
+    else
+    {
+        ARTEMIS_DEBUG_PRINTF("Data verification failed: Read data does not match written data.\n");
+    }
 
     am_util_delay_ms(500);
     while (1)
