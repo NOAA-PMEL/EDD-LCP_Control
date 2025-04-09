6e08a48b0b4deabbb8fff61593056dacfb540dd0
jkurina86 <joseph.kurina@noaa.gov>
2025-04-09 10:08:04 -0700

    Added CMakeLists.txt File
    Initial attempt

--- Firmware/LCP_Control/tools/gen_pins.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-export AMSDK=/c/version-control/SparkFun_Apollo3_AmbiqSuite_BSPs
-
-BSP_PATH=/c/version-control/EDD-LCP_Control/Firmware/LCP_Control/src/bsp
-BOARDS_FILE=/c/version-control/EDD-LCP_Control/Firmware/tools/configuration/boards.sh
-
-$AMSDK/common/tools_sfe/scripts/regen_bsp_pins.sh -r $BSP_PATH -b $BOARDS_FILE
